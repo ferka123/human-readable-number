f5afa1f305a82a65a702c2bebaec155fdd48de85
ferka123 <ferka911@gmail.com>
2022-06-27 17:54:49 +0300

    feat: reworked function

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
-module.exports = function toReadable (number) {
-    let resultStr='';
-    const strNum = number.toString();
-    const single = ['','one','two','three','four','five','six','seven','eight','nine'];
-    const teen = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
-    const dec = ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
-        if (strNum.length==1) resultStr = single[strNum[strNum.length-1]*1];
-        if (strNum.length>1) {
-            if (strNum[strNum.length-2]=='1')
-                resultStr = teen[strNum[strNum.length-1]*1] + resultStr
-            else
-                resultStr = dec[strNum[strNum.length-2]*1] + ' ' + single[strNum[strNum.length-1]*1] + resultStr
-        }
-        if (strNum.length>2) {
-            resultStr = single[strNum[strNum.length-3]*1] + ' hundred ' + resultStr;
-        }
-
-    resultStr = resultStr.replace(/  /g,' ').trim();
-    return resultStr=='' ? 'zero' : resultStr;   
+module.exports = function toReadable(n) {
+    const words = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven', 'twelve','thirteen','fourteen',
+    'fifteen','sixteen','seventeen','eighteen','nineteen','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety',' hundred '];
+    word = words[n/100|0] + words[(n>99)*28] + (n%100 < 20 ? words[n%100] : words[18+n/10%10|0] + ' ' + words[n%10]);
+    return n==0 ? 'zero' : word.trim();
 }
